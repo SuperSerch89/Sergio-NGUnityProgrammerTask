f2ccbb1927056c611ce56abe149a29bf83e04d3d
Sergio Peredo <snp.rogramer@gmail.com>
2025-06-01 16:33:40 -0400

    Added Initial Spaceship manager Setup. Placed initial art and collissions for reference.

---
+++ CheeseOdysseyProject/CheeseOdyssey/Assets/CheeseOdysseyAssets/Scripts/Managers/SpaceshipLevelManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SpaceshipLevelManager : LevelManager
+{
+    public override void StartScene()
+    {
+        throw new System.NotImplementedException();
+    }
+}
